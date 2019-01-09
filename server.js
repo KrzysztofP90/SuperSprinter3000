1ff7e45efc068a6517f2857274c5c915fe5b5b65
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-01-09 15:20:59 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.static('/styles'));
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+
+
+app.get('/', function (req, res) {
+   res.render('index', {title:'Super Sprinter 3000', message:'Welcome to Super Sprinter 3000!'})
+})
+
+var server = app.listen(8000, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+   
+   console.log("Start listening at http://%s:%s", host, port)
+})
