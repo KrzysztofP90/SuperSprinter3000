c2f0b25ea3915acbcbce12b815cb3fa2297c7cf8
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-01-09 21:30:49 +0100

    Implemented add user story template and navigation engine.

--- server.js
+++ server.js
@@ -1,18 +1,5 @@
 function readDataFromDataBaseFile() {
    var fs = require('fs');
-   // var csv = require("fast-csv");
-   // var stream = fs.createReadStream("DataBase/db.csv");
-   
-   // var csvStream = csv()
-   // .on("data", function(data){
-   //      console.log(data);
-   // })
-   // .on("end", function(){
-   //      console.log("done");
-   // });
-   // content = stream.pipe(csvStream);
-   // console.log(content + " dsadasfasfas");
-
    var jsonStringArray = fs.readFileSync('DataBase/db.txt').toString().split("$");
    var objectsArray = [];
    for (let i = 0; i < jsonStringArray.length; i++) {
@@ -26,6 +13,14 @@ var express = require('express');
 var app = express();
 
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
+app.use(bodyParser.json());
+
+
+
 app.use(express.static(__dirname + '/public'));
 app.use(express.static('/styles'));
 
@@ -37,6 +32,17 @@ app.set('view engine', 'pug');
 app.get('/', function (req, res) {
    var arrayOfRecordsToTable = readDataFromDataBaseFile();
    res.render('index', {arrayOfRecordsToTable});
+});
+
+
+app.get('/add', function(req, res) {
+   res.render('add');
+});
+
+
+app.post('/added', function(req, res) {
+   console.log(req.body.userStory);
+   res.render('added');
 })
 
 var server = app.listen(8000, function () {
@@ -44,5 +50,5 @@ var server = app.listen(8000, function () {
    var port = server.address().port;
    
    console.log("Start listening at http://%s:%s", host, port)
-})
+});
 
