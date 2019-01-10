a9c5663e462457d053780cb710f90ab26c707612
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-01-10 17:10:50 +0100

    Implemented edit user story functionality.

--- server.js
+++ server.js
@@ -4,23 +4,26 @@ function readDataFromDataBaseFile() {
    for (let i = 0; i < jsonStringArray.length; i++) {
       objectsArray.push(JSON.parse(jsonStringArray[i]));
    }
-   id = objectsArray.length;
+   countOfUserStory = objectsArray.length;
    return objectsArray;
 }
 
-function Story(title,userStory,criteria,value,estimation) {
-   this.id = id;
-   this.title = title;
-   this.story = userStory;
-   this.criteria = criteria;
-   this.value = value;
-   this.estimation = estimation;
-   this.status = "planning";
+
+function Story(parameters) {
+   this.id = parameters.get("id");
+   this.title = parameters.get("title");
+   this.story = parameters.get("story");
+   this.criteria = parameters.get("criteria");
+   this.value = parameters.get("value");
+   this.estimation = parameters.get("estimation");
+   this.status = parameters.get("status");
 }
 
+
 function saveNewRecordToDataBaseFile(title,userStory,criteria,value,estimation) {
-   id ++;
-   var newStory = new Story(title,userStory,criteria,value,estimation);
+   countOfUserStory ++;
+   var parametersMap = createParametersMapForNewUserStory(title,userStory,criteria,value,estimation);
+   var newStory = new Story(parametersMap);
    var newStoryJson = "$" + JSON.stringify(newStory);
    fs.appendFile('DataBase/db.txt', newStoryJson, function (err) {
       if (err) throw err;
@@ -29,11 +32,64 @@ function saveNewRecordToDataBaseFile(title,userStory,criteria,value,estimation) 
 }
 
 
+function createParametersMapForNewUserStory(title,userStory,criteria,value,estimation) {
+   var map = createParametersMapWithoutIdAndStatus(title,userStory,criteria,value,estimation);
+   map.set("id", countOfUserStory);
+   map.set("status", "planning");
+   return map;
+}
+
+
+function createParametersMapWithoutIdAndStatus(title,userStory,criteria,value,estimation) {
+   var map = new Map();
+   map.set("title", title);
+   map.set("story", userStory);
+   map.set("criteria", criteria);
+   map.set("value", value);
+   map.set("estimation", estimation);
+   return map;
+}
+
+function createParametersMapForEditedUserStory(id, title, story, criteria, value, estimation, status) {
+   var map = createParametersMapWithoutIdAndStatus(title, story, criteria, value, estimation);
+   map.set("id", id);
+   map.set("status", status);
+   return map;
+}
+
+
 function parseCookieToEdit(cookie) {
    var numberOfStoryToedit = cookie.split(':')[1];
    var length = numberOfStoryToedit.length;
    numberOfStoryToedit = numberOfStoryToedit.substring(1, length-2);
    return numberOfStoryToedit;
+}
+
+
+function editUserStory(id, title, story, criteria, value, estimation, status) {
+   var storyArray = readDataFromDataBaseFile();
+
+   var parametersMap = createParametersMapForEditedUserStory(id, title, story, criteria,
+       value, estimation, status);
+
+   var editedStory = new Story(parametersMap);
+   storyArray[id-1] = editedStory;
+   var newJsonDataBase = "";
+   for (let i = 0; i < storyArray.length; i++) {
+      newJsonDataBase += JSON.stringify(storyArray[i]);
+      if (i < storyArray.length - 1) {
+         newJsonDataBase += "$";
+      }
+   }  
+   saveNewJsonToDataBaseFile(newJsonDataBase);
+}
+
+
+function saveNewJsonToDataBaseFile(newJson) {
+   fs.writeFile('DataBase/db.txt', newJson, function (err) {
+      if (err) throw err;
+      console.log('Saved!');
+    });
 }
 
 
@@ -87,7 +143,7 @@ app.get('/add', function(req, res) {
 
 app.post('/added', function(req, res) {
    saveNewRecordToDataBaseFile(req.body.storyTitle, req.body.userStory, req.body.criteria, 
-      req.body.value,req.body.estimation);
+      req.body.value,req.body.estimation, req.body.status);
    res.render('added');
 })
 
@@ -101,6 +157,8 @@ app.post('/edited', function(req, res) {
    var cookie = JSON.stringify(cookieParser.JSONCookies(req.cookies));
    var numberOfStoryToEdit = parseCookieToEdit(cookie);
    res.clearCookie("toEdit");
+   editUserStory(numberOfStoryToEdit, req.body.storyTitle, req.body.userStory, req.body.criteria, 
+      req.body.value,req.body.estimation, req.body.status);
    res.render('edited');
 
 })
@@ -115,7 +173,7 @@ var server = app.listen(8000, function () {
 
 
 
-var id = 0;
+var countOfUserStory = 0;
 const fs = require('fs');
 
 
